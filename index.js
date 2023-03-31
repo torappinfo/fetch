d1685d750a6f1e1262b6d3047ee32f90e45d03fe
James Feng Cao <jamesfengcao@torapp.info>
2023-03-31 11:45:26 +0800

    use deno

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
-var http = require('http');
-http.createServer(function (req, res) {
-  var url = req.url.substring(1);
-  console.log(`request at ${url}!`)
-  fetch(url).then(res => res.text())
-    .then(text => {res.write(text);res.end();});
-}).listen(process.env.PORT || 8080);
+import { serve } from "https://deno.land/std@0.140.0/http/server.ts";
+serve((req) => {
+  let reqHeaders = new Headers(req.headers);
+  let fp = {
+    method: req.method,
+    headers: {}
+  }
+
+  let he = reqHeaders.entries();
+  for (let h of he) {
+    const key = h[0];
+    if( 'user-agent' == key)
+      fp.headers[key] = h[1];
+    else if( 'cookie1' == key)
+      fp.headers['Cookie'] = h[1];
+  }
+  return await fetch('https://www.bing.com/turing/conversation/create', fp);
+
+});
