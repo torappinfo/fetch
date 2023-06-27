6b8338434d2d1066e2a22e6304019d7370e24584
James Feng Cao <jamesfengcao@torapp.info>
2023-06-28 06:50:28 +0800

    init fp.headers in vanilla.js

--- vanilla.js
+++ vanilla.js
@@ -16,11 +16,7 @@ server.listen(port);
 async function goUrl(request, url) {
   let fp = {
     method: request.method,
-    headers: {}
+    headers: request.headers
   };
-  fp.headers = new Headers(request.headers);
-  for(var i = 2; i < arguments.length-1; i=i+2){
-    fp.headers[arguments[i]] = arguments[i+1];
-  }
   return await fetch(url, fp);
 }
