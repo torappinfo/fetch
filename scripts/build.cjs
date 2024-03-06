874fef0b6669782c1329282591bb68c1e15b5a9c
James Feng Cao <jamesfengcao@torapp.info>
2024-03-06 22:18:12 +0800

    fix build.cjs

--- scripts/build.cjs
+++ scripts/build.cjs
@@ -44,7 +44,8 @@ try {
             process.exit(1)
         }
         console.info('Successfully built app.js into .zeabur/output/functions/index.func')
-        fs.copyFileSync(render.js, '.zeabur/output/functions/index.func/index.js')
+        fs.copyFileSync(.zeabur/output/functions/index.func/render.js, '.zeabur/output/functions/index.func/index.js');
+        fs.rmSync('.zeabur/output/functions/index.func/render.js')
     })
 } catch (error) {
     console.error(error)
