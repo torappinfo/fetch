71964a62c09a7d083199ae5a85b5cb7921bbcf49
James Feng Cao <jamesfengcao@torapp.info>
2024-03-06 20:46:23 +0800

    build.cjs: mkdir recursively

--- scripts/build.cjs
+++ scripts/build.cjs
@@ -2,7 +2,7 @@ const fs = require('fs');
 const path = require('path');
 const zeaburO = '.zeabur/output/functions/index.func';
 if (!fs.existsSync(zeaburO)) {
-  fs.mkdirSync(zeaburO);
+  fs.mkdirSync(zeaburO,{recursive: true});
 }
 fs.cpSync('package.json', '.zeabur/output/functions/index.func/package.json')
 fs.cpSync('render.js', '.zeabur/output/functions/index.func/index.js')
