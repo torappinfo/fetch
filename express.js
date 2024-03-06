a80bbf3fc2702f51fcb5b1a6cc6271bdfd783def
James Feng Cao <jamesfengcao@torapp.info>
2024-03-06 22:33:17 +0800

    revert back to express.js

--- express.js
+++ express.js
@@ -1,5 +1,5 @@
-const express = require("express");
-const fetch = require('node-fetch');
+import express from "express";
+import fetch from 'node-fetch';
 const app = express();
 const port = process.env.PORT || 8080;
 
