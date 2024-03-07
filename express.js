f0f7d047c83be8086d1b9505ba760cdde11d2d79
James Feng Cao <jamesfengcao@torapp.info>
2024-03-07 23:07:37 +0800

    add log() to express.js

--- express.js
+++ express.js
@@ -8,6 +8,7 @@ app.get("/", async (req, res) => {
   let url = req.url;
   let iSlash = url.indexOf('?',11);
   let nUrl = url.substring(iSlash+1);
+  console.log(nUrl);
   let response = await goUrl(req, nUrl);
   res.status(response.status);
   res.set('Content-Type', response.headers.get('Content-Type'));
